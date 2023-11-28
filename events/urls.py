a43930d9fddd21e1efcf853c62e89e997d53d5c7
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-11-28 13:29:35 +0530

    Added Calender

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="home"),
+    # Path Converters:
+    # int: numbers
+    # str: strings
+    # path: whole urls /
+    # slug: hyphe-and_underscores_stuff
+    # UUID: Universally unique identifier
+    #path('',views.home, name="home"),
+    path('<int:year>/<str:month>/',views.home, name="home"),
 ]
--- events/views.py
+++ events/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+import calendar
+from calendar import HTMLCalendar
 # Create your views here.
-def home(request):
-    return render(request, 'home.html',{})
+def home(request,year,month):
+    name = "JOHN"
+    month = month.title() # Convert lowerCase to upperCase
+    # Convert month_name to month_number
+    month_number = list(calendar.month_name).index(month)
+    return render(request, 'home.html',{
+        "name" : name,
+        "year" : year,
+        "month" : month,
+        "month_number" : month_number,
+    })
