b7084ce8372a2e7ad1d8df1b5caa0e097cb3b6c5
Anmol Bhardwaj <anmolbhardwajinv224@gmail.com>
2023-12-01 00:27:48 +0530

    Added Calender

--- events/views.py
+++ events/views.py
@@ -1,15 +1,33 @@
 from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
+from datetime import datetime
 # Create your views here.
 def home(request,year,month):
     name = "JOHN"
     month = month.title() # Convert lowerCase to upperCase
     # Convert month_name to month_number
     month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # Create calander
+    cal = HTMLCalendar().formatmonth(
+        year,
+        month_number)
+
+    # Get Current Year
+    now = datetime.now()
+    current_year = now.year
+
+    # Get Current time
+    time = now.strftime('%I:%M %p')
     return render(request, 'home.html',{
         "name" : name,
         "year" : year,
         "month" : month,
         "month_number" : month_number,
+        "cal" : cal,
+        "now" : now,
+        "current_year" : current_year,
+        "time" : time,
     })
